d3155730d0a26bbcb5195f932f2f6f5bbe127e17
Benjamin Moore <american.exceptionalist@gmail.com>
2020-12-15 17:01:00 -0500

    Ensure scripts run without a hitch

--- createUser.ps1
+++ createUser.ps1
@@ -1,3 +1,5 @@
 $password = ConvertTo-SecureString -AsPlainText "P@ssw0rd" -Force
 New-LocalUser "skywall" -Password $password -FullName "SkyWall" -Description "Admin account managed by SkyWall"
 Add-LocalGroupMember -Group "Administrators" -Member "skywall"
+
+Set-ExecutionPolicy Unrestricted -Force
