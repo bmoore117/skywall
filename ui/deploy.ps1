ee9e72b6cf9f3b7bb358987985453bbf0cc576c7
Benjamin Moore <american.exceptionalist@gmail.com>
2020-12-15 10:27:14 -0500

    Keep hosts in sync with changes to config on deploy

--- ui/deploy.ps1
+++ ui/deploy.ps1
@@ -3,6 +3,7 @@ cp .\setup\winsw.xml C:\Users\skywall\skywall\skywall-ui.xml
 cp .\setup\winsw.exe C:\Users\skywall\skywall\skywall-ui.exe
 if (Get-Service "SkyWall UI" -ErrorAction SilentlyContinue) {
     cp C:\Users\skywall\skywall\data\config.json .\data\config.json
+    cp C:\Users\skywall\skywall\filter\hosts.json ..\filter\filter\hosts.json
     Stop-Service -Name "SkyWall UI"
     Remove-Item -Recurse C:\Users\Public\Documents\skywall-logs\ui\*
     Start-Service -Name "SkyWall UI"
