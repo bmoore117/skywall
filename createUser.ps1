b1fb10fc4138ea48c12707c0b5d390149452fb80
Benjamin Moore <american.exceptionalist@gmail.com>
2020-12-17 10:37:52 -0500

    Fix credential

--- createUser.ps1
+++ createUser.ps1
@@ -7,6 +7,6 @@ $password = ConvertTo-SecureString -AsPlainText "P@ssw0rd" -Force
 New-LocalUser "skywall" -Password $password -FullName "SkyWall" -Description "Admin account managed by SkyWall"
 Add-LocalGroupMember -Group "Administrators" -Member "skywall"
 
-$cred = New-Credential "skywall" $password
+$cred = New-Object System.Management.Automation.PSCredential ("skywall", $password)
 $cwd = Get-Location
 Start-Process powershell.exe -WorkingDirectory $cwd -ArgumentList "-File",".\doInstall.ps1"
