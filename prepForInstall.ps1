b7018a33be64394c07905f3caaa5a0b772683ff8
Benjamin Moore <american.exceptionalist@gmail.com>
2020-12-17 17:54:31 -0800

    Theoretical fix for invalid directory

--- prepForInstall.ps1
+++ prepForInstall.ps1
@@ -4,12 +4,15 @@ if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
 }
 
 if ($PSScriptRoot -ne 'C:\Users\Public\skywall') {
+    Write-Host "Copying files to Public"
     cp -Recurse . C:\Users\Public\skywall
 }
 
+Write-Host "Creating skywall user"
 $password = ConvertTo-SecureString -AsPlainText "P@ssw0rd" -Force
 New-LocalUser "skywall" -Password $password -FullName "SkyWall" -Description "Admin account managed by SkyWall"
 Add-LocalGroupMember -Group "Administrators" -Member "skywall"
 
+cd ~
 $cred = New-Object System.Management.Automation.PSCredential ("skywall", $password)
 Start-Process powershell.exe -Credential $cred -ArgumentList "Start-Process powershell.exe -ArgumentList '-File','C:\Users\Public\skywall\doInstall.ps1' -Verb RunAs"
