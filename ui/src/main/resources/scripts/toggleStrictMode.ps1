3ffc2ffaf2e334faa3919c35dfcfaa3c17ec2274
Benjamin Moore <moore.benjamin@live.com>
2021-04-08 21:43:19 -0400

    Change backup admin username to sort alphabetically below the primary skywall account; update config

--- ui/src/main/resources/scripts/toggleStrictMode.ps1
+++ ui/src/main/resources/scripts/toggleStrictMode.ps1
@@ -9,7 +9,7 @@
 if ($mode -eq "on") {
     $returnUsers = [System.Collections.Generic.List[string]]::new()
     Disable-LocalUser -Name "Administrator"
-    $users = Get-LocalUser | where {$_.Name -ne "skywall" -and $_.Name -ne "backup-admin"}
+    $users = Get-LocalUser | where {$_.Name -ne "skywall" -and $_.Name -ne "skywall-backup"}
     foreach ($user in $users) {
         if ($user.Enabled -eq $true) {
             Remove-LocalGroupMember -Group "Administrators" -Member $user.Name
