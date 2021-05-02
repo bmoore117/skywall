0a11ad0de666196bf05ed9b83969b7544b64e406
Benjamin Moore <moore.benjamin@live.com>
2021-05-02 18:41:58 -0400

    Ensure proxy can't be jammed on startup by blocking its default ports

--- filter/deploy.ps1
+++ filter/deploy.ps1
@@ -6,6 +6,7 @@ cp .\skywall-filter.py $installDir
 cp .\launchProxy.ps1 $installDir
 cp .\mitmdump.py $installDir
 cp .\setup\winsw.xml $installDir\skywall-filter.xml
+cp ..\dist\Python39\Lib\site-packages\mitmproxy\platform\windows.py $installDir\dist\Python39\Lib\site-packages\mitmproxy\platform
 if (Get-Service "SkyWall Filter" -ErrorAction SilentlyContinue) {
     Remove-Item -Recurse C:\Users\Public\Documents\skywall-logs\filter\*
     Start-Service -Name "SkyWall Filter"
--- filter/launchProxy.ps1
+++ filter/launchProxy.ps1
@@ -1,4 +1,6 @@
-﻿$jsonLocation = "~\AppData\Local\SkyWall\filter\hosts\hosts.json"
+﻿
+
+$jsonLocation = "~\AppData\Local\SkyWall\filter\hosts\hosts.json"
 $json = Get-Content $jsonLocation | ConvertFrom-Json
 if ($json.filterActive -eq $false) {
     Write-Host "Aborting startup as filter set to OFF, re-enable from UI"
@@ -30,10 +32,28 @@ Set-Location $PSScriptRoot
 $json = Get-Content $jsonLocation | ConvertFrom-Json
 $ignoredHosts = $json.ignoredHosts -join "|"
 
+# make sure no one thought they'd be clever and jam the proxy on startup by claiming the ports
+$startingPort = 8080 # default taken from windows.py
+$connections = Get-NetTCPConnection -LocalPort $startingPort -ErrorAction SilentlyContinue
+while ($connections -ne $null) {
+    $startingPort = $startingPort + 1
+    $connections = Get-NetTCPConnection -LocalPort $startingPort -ErrorAction SilentlyContinue
+}
+$startingPort | Out-File -FilePath $env:LOCALAPPDATA\SkyWall\port.txt -Encoding ascii
+
+$redirectAPIPort = 8085 # default taken from windows.py
+$connections = Get-NetTCPConnection -LocalPort $redirectAPIPort -ErrorAction SilentlyContinue
+while ($connections -ne $null) {
+    $redirectAPIPort = $redirectAPIPort + 1
+    $connections = Get-NetTCPConnection -LocalPort $redirectAPIPort -ErrorAction SilentlyContinue
+}
+$redirectAPIPort | Out-File -FilePath $env:LOCALAPPDATA\SkyWall\redirectport.txt -Encoding ascii
+
+
 if ([string]::IsNullOrEmpty($ignoredHosts)) {
     Write-Host "Launching with no ignored hosts"
-    .\dist\Python39\python.exe .\mitmdump.py --mode transparent --set block_global=false --ssl-insecure --no-http2 -s .\skywall-filter.py
+    .\dist\Python39\python.exe .\mitmdump.py --mode transparent --set block_global=false --ssl-insecure --no-http2 -s .\skywall-filter.py --listen-port $startingPort
 } else {
     Write-Host "Launching with ignored hosts" $ignoredHosts
-    .\dist\Python39\python.exe .\mitmdump.py --mode transparent --set block_global=false --ssl-insecure --no-http2 -s .\skywall-filter.py --ignore-hosts $ignoredHosts
+    .\dist\Python39\python.exe .\mitmdump.py --mode transparent --set block_global=false --ssl-insecure --no-http2 -s .\skywall-filter.py --ignore-hosts $ignoredHosts --listen-port $startingPort
 }
