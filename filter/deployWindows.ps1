58424944bdd32e389e0c9279e931d2a0d4d1c3dc
Benjamin Moore <moore.benjamin@live.com>
2021-09-04 19:33:16 -0400

    Utility script to deploy windows.py

---
+++ filter/deployWindows.ps1
@@ -0,0 +1,9 @@
+$installDir = "C:\Program Files\SkyWall"
+if (Get-Service "SkyWall Filter" -ErrorAction SilentlyContinue) {
+    Stop-Service -Name "SkyWall Filter"
+}
+cp C:\Users\moore\Code\skywall\dist\Python39\Lib\site-packages\mitmproxy\platform\windows.py $installDir\dist\Python39\Lib\site-packages\mitmproxy\platform
+if (Get-Service "SkyWall Filter" -ErrorAction SilentlyContinue) {
+    Remove-Item -Recurse C:\Users\Public\Documents\skywall-logs\filter\*
+    Start-Service -Name "SkyWall Filter"
+}
