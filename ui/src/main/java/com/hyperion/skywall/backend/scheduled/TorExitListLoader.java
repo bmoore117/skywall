655906d777bc6fdbcebbcd4a34a1dd937c80c009
Benjamin Moore <moore.benjamin@live.com>
2021-09-12 14:17:15 -0400

    Ignore SSL, move to 1 hour interval to fix errors

--- ui/src/main/java/com/hyperion/skywall/backend/scheduled/TorExitListLoader.java
+++ ui/src/main/java/com/hyperion/skywall/backend/scheduled/TorExitListLoader.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -16,11 +19,28 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 @Component
 public class TorExitListLoader {
 
     private static final Logger log = LoggerFactory.getLogger(TorExitListLoader.class);
+
+    private static final TrustManager[] trustAllCerts = new TrustManager[]{
+            new X509TrustManager() {
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public void checkClientTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+                public void checkServerTrusted(
+                        java.security.cert.X509Certificate[] certs, String authType) {
+                }
+            }
+    };
 
     private final WinUtils winUtils;
 
@@ -30,7 +50,7 @@ public class TorExitListLoader {
     }
 
     // every 30 minutes, initial delay of 10s to not conflict with script extraction in StartupTaskRunner
-    @Scheduled(fixedDelay = 1800000L, initialDelay = 10000L)
+    @Scheduled(fixedDelay = 3600000L, initialDelay = 10000L)
     public void downloadTorExitList() {
         String getEndpoint = "https://www.dan.me.uk/torlist/";
 
@@ -39,9 +59,13 @@ public class TorExitListLoader {
                 .GET()
                 .build();
 
-        var client = HttpClient.newHttpClient();
-
         try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, trustAllCerts, new SecureRandom());
+            HttpClient client = HttpClient.newBuilder()
+                    .sslContext(sslContext)
+                    .build();
+
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             if (response.statusCode() == HttpStatus.OK.value()) {
                 Path file = PathUtil.getWindowsPath("tor", "nodes.txt");
@@ -55,7 +79,7 @@ public class TorExitListLoader {
             } else {
                 log.warn("Unable to reach tor node list, perhaps you hit the 30 minute window. Http status: {}", response.statusCode());
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException | InterruptedException | NoSuchAlgorithmException | KeyManagementException e) {
             log.error("Error fetching tor exit list", e);
         }
     }
